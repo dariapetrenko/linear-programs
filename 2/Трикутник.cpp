1312cf810ef4a93b17a97f2f6d4003f537ecd937
dariapetrenko <118117503+dariapetrenko@users.noreply.github.com>
2022-11-27 20:25:36 +0200

    Add files via upload

---
+++ 2/Трикутник.cpp
@@ -0,0 +1,27 @@
+#include <iostream>;
+#include <cmath>;
+
+using namespace std;
+
+int main()
+{
+    double x1 = 0.0;
+    double y1 = 0.0;
+    double x2 = 1.0;
+    double y2 = 0.0;
+    double x3 = -1.0;
+    double y3 = 2.0;
+
+    //Формула Герона
+    double a = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    double b = sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
+    double c = sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+
+    double p = (a + b + c) / 2.0; // периметр 
+    double square = sqrt(p * (p - a) * (p - b) * (p - c)); // площа 
+
+    double h = square / 0.5; // висота 
+    cout << h;
+
+    return 0;
+}
