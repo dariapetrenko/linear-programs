7289427863e1014280c77f13d950767def7940d9
dariapetrenko <118117503+dariapetrenko@users.noreply.github.com>
2022-12-01 01:55:25 +0200

    Add files via upload

---
+++ 4/Функція.cpp
@@ -0,0 +1,33 @@
+/*Скласти структурну схему алгоритму та програму обчислення значень
+функції y=F(x) відповідно до завдань, зазначених у таблиці.
+Примітка. Значення першого з двох параметрів, які наведені в таблиці,
+задати як константу, значення другого ввести з клавіатури*/
+
+#include <iostream>
+#include <cmath>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	double const  t = 2;
+	double b, y, x, a;
+
+	cout << "b = ";
+	cin >> b;
+	cout << " " << endl;
+
+	a = exp(t + b);
+	cout << "Перший вираз a = " << a << endl;
+	cout << " " << endl;
+
+	x = sqrt(a + b);
+	cout << "Другий вираз x = " << x << endl;
+	cout << " " << endl;
+
+	y = pow(log10(abs(x + a)), 2);
+	cout << "Третій вираз y = " << y << endl;
+
+	return 0;
+}
